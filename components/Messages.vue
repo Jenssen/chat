282ad6a02f633317380f8169be86ede51eeb7725
Robert Jenssen <jenssen.90@gmail.com>
2017-06-04 22:23:38 +0200

    Bug Testing

--- components/Messages.vue
+++ components/Messages.vue
@@ -114,8 +114,9 @@ export default {
     flex-direction: column;
     padding-left: 10px;
     padding-top: 5px;
-    margin-bottom: 41px;
-    height: 100%;
+    height: calc(100% - 36px);
+    width: 100vw;
+    position: absolute;
   }
   .footer-container {
     display: flex;
