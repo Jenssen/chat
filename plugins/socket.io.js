7bcb62db0eb91c8e23ce1847729c485683060411
Robert Jenssen <jenssen.90@gmail.com>
2017-10-24 22:14:00 +0200

    Changed domain name

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -5,7 +5,7 @@ var socket
 if (process.env.NODE_ENV === 'development') {
   socket = io('127.0.0.1:3000')
 } else if (process.env.NODE_ENV === 'production') {
-  socket = io('jenssen.me')
+  socket = io('chat.jenssen.me')
 }
 
 export default socket
